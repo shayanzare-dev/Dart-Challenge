375b7475f3cc6241d4e3131cc2a6fbb589d5ad56
shayan <shayanzare2222@gmail.com>
2024-08-05 09:21:23 +0330

    Please enter the commit e for your changes. Lines add code example for practice.

--- lib/kar_amoozi/oop/1.dart
+++ lib/kar_amoozi/oop/1.dart
@@ -1,3 +1,15 @@
 void main() {
-  assert(2 > 1, 'hello');
+  const Bmv bmv = Bmv('assa', 'brand');
+  const Car benz = Car('Ass', 'vad');
+  print(bmv is Car);
+}
+
+class Car {
+  final String name, brand;
+
+  const Car(this.name, this.brand);
+}
+
+class Bmv extends Car {
+  const Bmv(super.name, super.brand);
 }
