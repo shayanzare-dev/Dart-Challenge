09d0683c4112c8130280cc457ad3e2036ab08a79
shayan <shayanzare2222@gmail.com>
2024-08-13 22:58:08 +0330

    add customer managemnt files(not completed)

---
+++ lib/kar_amoozi/oop/customer_management_system/company.dart
@@ -0,0 +1,12 @@
+import 'customer/customer.dart';
+
+class Company {
+  final int id;
+  final String name;
+
+  Company(this.id, this.name) {
+    final Customer customer = Customer();
+  }
+
+
+}
--- lib/kar_amoozi/oop/customer_management_system/customer/customer.dart
+++ lib/kar_amoozi/oop/customer_management_system/customer/customer.dart
@@ -1,1 +1,5 @@
-class Customer {}
+abstract class Customer {
+  factory Customer() {
+    return Customer();
+  }
+}
